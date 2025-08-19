0550966fa085a849bf9b97974b44f85793e6591f
Hai Mat <mathoanghai92@gmail.com>
2025-08-19 22:01:13 +0700

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,11 @@
+/** @format */
+
+export const AppConfig = {
+  apiUrl: "http://api-beeshoes.devopsedu.vn",
+  routerBase: "",
+};
+
+export const AppConfigAddress = {
+  apiUrl: "",
+  routerBase: "",
+};
